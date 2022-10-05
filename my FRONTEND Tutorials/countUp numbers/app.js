ffeeb4f3ce73e3e3d6e434bdde627cd0bc0d635b
yhyawy <yhyawy1@gmail.com>
2022-10-05 19:38:00 +0200

    Finished countUp App and updated HTML stuf sheet

--- my FRONTEND Tutorials/countUp numbers/app.js
+++ my FRONTEND Tutorials/countUp numbers/app.js
@@ -1,27 +1,44 @@
 const counter = document.querySelector(".counter");
 
-const upTime = () => {
-  let target = +counter.dataset.target;
-  let shown = +counter.innerText;
-  let increment = target / 100;
-  if (shown < target) {
-    counter.innerText = `${Math.ceil(shown + increment)}`;
-    setTimeout(upTime, increment);
-  }
-};
-upTime();
+//Youtube
+// const upTime = () => {
+//    let target = counter.dataset.target;
+//    let shown = +counter.innerText;
+//    let increment = target / 100;
+//    if (shown < target) {
+//       counter.innerText = `${Math.ceil(shown + increment)}`;
+//       setTimeout(upTime, increment);
+//    }
+// };
+// upTime();
 
+//Yhay
+function upCounter() {
+   let target = counter.dataset.target;
+   let innerText = counter.innerText;
+
+   let countUp = setInterval(function () {
+      if (innerText < target) {
+         innerText++;
+         counter.innerText = innerText;
+      } else {
+         clearInterval(countUp);
+      }
+   }, 1);
+}
+upCounter();
+
+//Osama Elzero
 // function upCounter() {
-//   let target = counter.dataset.target;
-//   let innerText = counter.innerText;
+//    let target = counter.dataset.target;
+//    let innerText = counter.innerText;
 
-//   let countUp = setInterval(function () {
-//     if (innerText < target) {
+//    let countUp = setInterval(function () {
 //       innerText++;
 //       counter.innerText = innerText;
-//     } else {
-//       clearInterval(countUp);
-//     }
-//   }, 1);
+//       if (innerText == target) {
+//          clearInterval(countUp);
+//       }
+//    }, 1000 / target);
 // }
 // upCounter();
