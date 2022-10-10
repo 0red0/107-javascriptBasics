375f32fe162ef5d71770fa1c4699e101db09c329
yhyawy <yhyawy1@gmail.com>
2022-10-10 20:09:04 +0200

    Memory game done

--- my FRONTEND Tutorials/Memory Game/mem.js
+++ my FRONTEND Tutorials/Memory Game/mem.js
@@ -1,38 +1,31 @@
 // To Create Random colors
-let clrsArr = [
-   "0",
-   "1",
-   "2",
-   "3",
-   "4",
-   "5",
-   "6",
-   "7",
-   "8",
-   "9",
-   "a",
-   "b",
-   "c",
-   "d",
-   "f",
-];
+// prettier-ignore
+let clrsArr = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","f",];
 
 // Splash Screen Handle
 const startBtn = document.querySelector("nav span");
+const inputName = document.querySelector("nav input");
+const triesElement = document.querySelector(".tries span");
+const successSfx = document.getElementById("success");
+const failSfx = document.getElementById("fail");
+const endSfx = document.getElementById("end-game");
+
 startBtn.onclick = function () {
-   let yourName = prompt("What's Your Name?");
-   if (yourName == null || yourName == "") {
-      document.querySelector(".name span").innerText = "Unknown";
+   if (inputName.value == null || inputName.value == "") {
+      document.querySelector(".name span").innerText = "Mr. No-name";
    } else {
-      document.querySelector(".name span").innerText = yourName;
+      document.querySelector(".name span").innerText = inputName.value;
    }
    document.querySelector("nav").remove();
 };
 
 let duration = 1000;
+let counter = 0;
 const blocksContainer = document.querySelector(".memory-game-blocks");
 const blocks = Array.from(blocksContainer.children);
+
 let orderRange = [...blocks.keys()];
+shuffle(orderRange);
 
 // Add order property to blocks
 blocks.forEach((b, index) => {
@@ -40,7 +33,84 @@ blocks.forEach((b, index) => {
    for (let i = 0; i < 6; i++) {
       clr += clrsArr[Math.floor(Math.random() * clrsArr.length)];
    }
-   b.style.order = index;
+   b.style.order = orderRange[index];
    b.style.color = clr;
-   console.log(clr);
+   b.addEventListener("click", function () {
+      flipBlock(b);
+   });
 });
+
+// Flip block function
+function flipBlock(selectedBlock) {
+   selectedBlock.classList.add("flipped");
+   let allFlippedBlocks = blocks.filter((flippedBlock) =>
+      flippedBlock.classList.contains("flipped")
+   );
+
+   if (allFlippedBlocks.length === 2) {
+      // Check 2 blocks only
+
+      stopClicking();
+      checkMatches(allFlippedBlocks[0], allFlippedBlocks[1]);
+   }
+}
+
+// Stop clicking
+function stopClicking() {
+   blocksContainer.classList.add("no-clicking");
+   setTimeout(() => {
+      blocksContainer.classList.remove("no-clicking");
+   }, duration);
+}
+
+// Check Matching blocks
+function checkMatches(firstBlock, secondBlock) {
+   if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
+      firstBlock.classList.remove("flipped");
+      secondBlock.classList.remove("flipped");
+      firstBlock.classList.add("has-match");
+      secondBlock.classList.add("has-match");
+      successSfx.play();
+      counter++;
+      console.log(counter);
+      if (counter === blocks.length / 2) {
+         wining();
+      }
+   } else {
+      triesElement.innerText = parseInt(triesElement.innerText) + 1;
+      setTimeout(() => {
+         firstBlock.classList.remove("flipped");
+         secondBlock.classList.remove("flipped");
+      }, duration);
+      failSfx.play();
+   }
+}
+
+// Shuffle Array
+function shuffle(array) {
+   let current = array.length;
+   let random;
+   let temp;
+
+   while (current > 0) {
+      random = Math.floor(Math.random() * current);
+      current--;
+      temp = array[current];
+      array[current] = array[random];
+      array[random] = temp;
+   }
+   return array;
+}
+
+// win scenario
+function wining() {
+   let inter = setInterval(() => {
+      blocks.forEach((bl) => {
+         bl.classList.toggle("has-match");
+      });
+   }, 150);
+   setTimeout(() => {
+      clearInterval(inter);
+   }, 1500);
+   endSfx.play();
+}
